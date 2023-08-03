85167e14cbe80182db61c275ae103e3f743bfd59
Amirol Zolkifli <sitehandy@gmail.com>
2023-08-03 12:53:05 +0800

    Add jawatan

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger('bahagian_id')->nullable();
+            $table->string('jawatan')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
