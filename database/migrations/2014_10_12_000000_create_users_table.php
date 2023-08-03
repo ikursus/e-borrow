9c1b3ad8ebec801d31fb2703020d889ace85d9ce
Amirol Zolkifli <sitehandy@gmail.com>
2023-08-03 12:51:59 +0800

    Latest updates

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('bahagian_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            // Foreign key
+            $table->foreign('bahagian_id')->references('id')->on('bahagian');
         });
     }
 
--- database/migrations/2023_08_02_081159_create_permohonan_table.php
+++ database/migrations/2023_08_02_081159_create_permohonan_table.php
@@ -14,10 +14,30 @@ return new class extends Migration
         Schema::create('permohonan', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('pegawai_pemohon_id')->unsigned();
-            $table->unsignedBigInteger('pegawai_bertanggungjawab_id');
+            $table->unsignedBigInteger('pegawai_bertanggungjawab_id')->nullable();
             $table->date('tarikh_jangka_pinjam')->nullable();
             $table->date('tarikh_jangka_pulang')->nullable();
+            $table->text('tujuan_permohonan')->nullable();
+            $table->string('lokasi_tujuan')->nullable();
+            $table->unsignedBigInteger('pengawai_pengesah_id')->nullable();
+            $table->date('tarikh_pengesahan')->nullable();
+            $table->unsignedBigInteger('pengawai_pengeluar_id')->nullable();
+            $table->date('tarikh_pengeluaran')->nullable();
+            $table->unsignedBigInteger('pengawai_pengambil_id')->nullable();
+            $table->date('tarikh_ambil')->nullable();
+            $table->unsignedBigInteger('pengawai_pemulang_id')->nullable();
+            $table->date('tarikh_pulangan')->nullable();
+            $table->unsignedBigInteger('pengawai_penerima_pulangan_id')->nullable();
+            $table->date('tarikh_terima_pulangan')->nullable();
+            $table->text('catatan_pegawai_penerima')->nullable();
+            $table->string('status')->default('DRAF');
             $table->timestamps();
+            $table->softDeletes();
+
+            // Setkan foreign key untuk column pegawai_pemohon_id
+            // Supaya data yang masuk ke colum ini, perlu merujuk kepada kewujudan data
+            // di column id di table users
+            $table->foreign('pegawai_pemohon_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
