101b340036da2443de0f17bacc25da024bb6f693
Amirol Zolkifli <sitehandy@gmail.com>
2023-08-07 14:54:47 +0800

    Update modal delete user

--- resources/views/users/template-senarai.blade.php
+++ resources/views/users/template-senarai.blade.php
@@ -77,7 +77,11 @@
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
                                         <div class="modal-body">
-                                        Adakah anda bersetuju untuk menghapuskan / padam data ini?
+                                            Adakah anda bersetuju untuk menghapuskan / padam data ini?
+
+                                            <ul>
+                                                <li>Nama: {{ $staff->name }}</li>
+                                            </ul>
                                         </div>
                                         <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
