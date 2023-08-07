1173167edeeb85f10bfaa951ba72e00a694fa31c
Amirol Zolkifli <sitehandy@gmail.com>
2023-08-07 12:12:38 +0800

    Update template user

--- resources/views/users/template-detail.blade.php
+++ resources/views/users/template-detail.blade.php
@@ -2,51 +2,54 @@
 
 @section('isi-kandungan-utama')
 
-    <h1 class="mt-4">
-        {{ $pageTitle ?? 'Detail Profile' }}
-    </h1>
-    <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">Profile</li>
-    </ol>
+<h1 class="mt-4">
+    {{ $pageTitle ?? 'Detail Profile' }}
+</h1>
 
-    <div class="card mb-4">
-        <div class="card-header">
-            <i class="fas fa-table me-1"></i>
-            Profile Profile {{ $staff->name }}
+<div class="row">
+    <div class="col-xl-12">
+
+        <div class="card mb-4">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                Profile {{ $staff->name }}
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>PERKARA</th>
+                            <th>BUTIRAN</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr>
+                            <td>Nama</td>
+                            <td>{{ $staff->name }}</td>
+                        </tr>
+
+                        <tr>
+                            <td>Email</td>
+                            <td>{{ $staff->email }}</td>
+                        </tr>
+
+                        <tr>
+                            <td>Jawatan</td>
+                            <td>{{ $staff->jawatan }}</td>
+                        </tr>
+
+                        <tr>
+                            <td>Telefon</td>
+                            <td>{{ $staff->telefon }}</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div class="card-body">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>PERKARA</th>
-                        <th>BUTIRAN</th>
-                    </tr>
-                </thead>
-                <tbody>
 
-                    <tr>
-                        <td>Nama</td>
-                        <td>{{ $staff->name }}</td>
-                    </tr>
-
-                    <tr>
-                        <td>Email</td>
-                        <td>{{ $staff->email }}</td>
-                    </tr>
-
-                    <tr>
-                        <td>Jawatan</td>
-                        <td>{{ $staff->jawatan }}</td>
-                    </tr>
-
-                    <tr>
-                        <td>Telefon</td>
-                        <td>{{ $staff->telefon }}</td>
-                    </tr>
-
-                </tbody>
-            </table>
-        </div>
     </div>
+</div>
 
 @endsection
--- resources/views/users/template-edit.blade.php
+++ resources/views/users/template-edit.blade.php
@@ -1,58 +1,74 @@
 @extends('layout.induk')
 
 @section('isi-kandungan-utama')
+
 <h1 class="mt-4">
-    Kemaskini Rekod {{ $user->name }}
+    Kemaskini Rekod {{ $staff->name }}
 </h1>
 
 <div class="row">
     <div class="col-xl-12">
-        <div class="card">
-            <div class="card-body">
 
-                <form method="POST" action="{{ route('users.update', $user->id) }}">
-                    @csrf
-                    @method('PATCH')
+        <form method="POST" action="{{ route('users.update', $staff->id) }}">
+            @csrf
+            @method('PATCH')
 
-                    <div class="mb-3">
-                      <label class="form-label">Name</label>
-                      <input type="text" class="form-control" name="name" value="{{ $user->name }}">
-                    </div>
+            <div class="card">
+                <div class="card-body">
 
-                    <div class="mb-3">
-                      <label class="form-label">Email</label>
-                      <input type="text" class="form-control" name="email" value="{{ $user->email }}">
-                    </div>
+                        <div class="mb-3">
+                        <label class="form-label">Name</label>
+                        <input type="text" class="form-control" name="name" value="{{ old('name') ?? $staff->name }}">
+                        </div>
 
-                    <div class="mb-3">
-                      <label class="form-label">Bahagian</label>
-                      <input type="text" class="form-control" name="bahagian_id" value="{{ $user->bahagian_id }}">
-                    </div>
+                        <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="text" class="form-control" name="email" value="{{ old('email') ?? $staff->email }}">
+                        </div>
 
-                    <div class="mb-3">
-                      <label class="form-label">Jawatan</label>
-                      <input type="text" class="form-control" name="jawatan" value="{{ $user->jawatan }}">
-                    </div>
+                        <div class="mb-3">
+                            <label class="form-label">Bahagian</label>
+                            <select name="bahagian_id">
 
-                    <div class="mb-3">
-                      <label class="form-label">Telefon</label>
-                      <input type="text" class="form-control" name="telefon" value="{{ $user->telefon }}">
-                    </div>
+                                <option value="">-- Sila Pilih --</option>
+
+                                @foreach ($senaraiBahagian as $bahagian)
+                                <option value="{{ $bahagian->id }}" {{ $staff->bahagian_id == $bahagian->id ? ' selected="selected"' : NULL }}>
+                                    {{ $bahagian->nama }}
+                                </option>
+                                @endforeach
+
+                            </select>
 
 
-                    <div class="mb-3">
-                        <label class="form-label">Password</label>
-                        <input type="password" class="form-control" name="password">
-                      </div>
+                        </div>
 
+                        <div class="mb-3">
+                        <label class="form-label">Jawatan</label>
+                        <input type="text" class="form-control" name="jawatan" value="{{ old('jawatan') ?? $staff->jawatan }}">
+                        </div>
+
+                        <div class="mb-3">
+                        <label class="form-label">Telefon</label>
+                        <input type="text" class="form-control" name="telefon" value="{{ old('telefon') ?? $staff->telefon }}">
+                        </div>
+
+
+                        <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input type="password" class="form-control" name="password">
+                        </div>
+
+
+
+                </div>
+                <div class="card-footer">
                     <button type="submit" class="btn btn-primary">Save</button>
-                </form>
-
-
+                </div>
             </div>
-            <div class="card-footer">
 
-            </div>
-        </div>
+        </form>
+
     </div>
+</div>
 @endsection
